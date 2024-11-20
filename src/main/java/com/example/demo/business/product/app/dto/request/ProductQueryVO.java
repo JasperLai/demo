2591d9298a5c9a658053527bbdab9da0ca58828c
laimubin <laimubin@cmbc.com.cn>
2024-11-20 20:05:49 +0800

    完善检索条件

--- src/main/java/com/example/demo/business/product/app/dto/request/ProductQueryVO.java
+++ src/main/java/com/example/demo/business/product/app/dto/request/ProductQueryVO.java
@@ -7,4 +7,44 @@ public class ProductQueryVO {
     private String bondVariety;
     private String recmdFlag;
 
+    public String getProducdId() {
+        return producdId;
+    }
+
+    public void setProducdId(String producdId) {
+        this.producdId = producdId;
+    }
+
+    public String getBondCode() {
+        return bondCode;
+    }
+
+    public void setBondCode(String bondCode) {
+        this.bondCode = bondCode;
+    }
+
+    public String getBondName() {
+        return bondName;
+    }
+
+    public void setBondName(String bondName) {
+        this.bondName = bondName;
+    }
+
+    public String getBondVariety() {
+        return bondVariety;
+    }
+
+    public void setBondVariety(String bondVariety) {
+        this.bondVariety = bondVariety;
+    }
+
+    public String getRecmdFlag() {
+        return recmdFlag;
+    }
+
+    public void setRecmdFlag(String recmdFlag) {
+        this.recmdFlag = recmdFlag;
+    }
+
 }
