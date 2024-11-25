01ce75767037f6812b6e7f21f0e717bd8b76f36a
laimubin <laimubin@cmbc.com.cn>
2024-11-25 22:59:41 +0800

    完善异常处理框架逻辑

--- src/main/java/com/example/demo/common/catchall/CatchLogAspect.java
+++ src/main/java/com/example/demo/common/catchall/CatchLogAspect.java
@@ -7,25 +7,19 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.example.demo.common.exception.BizException;
-
 
 @Aspect
 @Component
 @EnableAspectJAutoProxy
 public class CatchLogAspect {
-
-
     private static final Logger log = LogManager.getLogger(CatchLogAspect.class);
 
-    /**
-     * The syntax of pointcut : https://blog.csdn.net/zhengchao1991/article/details/53391244
-     */
+    @Autowired
+    private DefaultResponseHandler responseHandler;
+
     @Pointcut("@within(CatchAndLog) && execution(public * *(..))")
     public void pointcut() {
     }
@@ -40,7 +34,8 @@ public class CatchLogAspect {
         try {
             response = joinPoint.proceed();
         } catch (Throwable e) {
-            response = handleException(joinPoint, e);
+            MethodSignature ms = (MethodSignature) joinPoint.getSignature();
+            response = responseHandler.handle(ms.getReturnType(), e);
         } finally {
             logResponse(startTime, response);
         }
@@ -48,30 +43,13 @@ public class CatchLogAspect {
         return response;
     }
 
-    private Object handleException(ProceedingJoinPoint joinPoint, Throwable e) {
-        MethodSignature ms = (MethodSignature) joinPoint.getSignature();
-        @SuppressWarnings("rawtypes")
-        Class returnType = ms.getReturnType();
-
-        if (e instanceof BizException) {
-            log.warn("BIZ EXCEPTION : " + e.getMessage());       
-            return new DefaultResponseHandler().handle(returnType, ((BizException) e).getErrCode(), e.getMessage());
-        }
-
-        log.error("UNKNOWN EXCEPTION :");
-        log.error(e.getMessage(), e);
-
-        return new DefaultResponseHandler().handle(returnType, "UNKNOWN_ERROR", e.getMessage());
-    }
-
     private void logResponse(long startTime, Object response) {
         try {
             long endTime = System.currentTimeMillis();
-            log.debug("RESPONSE : " + JSON.toJSONString(response));
-            log.debug("COST : " + (endTime - startTime) + "ms");
+            log.debug("RESPONSE : {}", response);
+            log.debug("COST : {}ms", (endTime - startTime));
         } catch (Exception e) {
-            //swallow it
-            log.error("logResponse error : " + e);
+            log.error("记录响应日志失败", e);
         }
     }
 
@@ -80,15 +58,13 @@ public class CatchLogAspect {
             if (!log.isDebugEnabled()) {
                 return;
             }
-            log.debug("START PROCESSING: " + joinPoint.getSignature().toShortString());
+            log.debug("START PROCESSING: {}", joinPoint.getSignature().toShortString());
             Object[] args = joinPoint.getArgs();
             for (Object arg : args) {
-                log.debug("REQUEST : " + JSON.toJSONString(arg, SerializerFeature.IgnoreErrorGetter));
+                log.debug("REQUEST : {}", arg);
             }
         } catch (Exception e) {
-            //swallow it
-            log.error("logReqeust error : " + e);
+            log.error("记录请求日志失败", e);
         }
     }
-
 }
--- src/main/java/com/example/demo/common/catchall/DefaultResponseHandler.java
+++ src/main/java/com/example/demo/common/catchall/DefaultResponseHandler.java
@@ -2,27 +2,67 @@ package com.example.demo.common.catchall;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+
+import com.example.demo.common.exception.BizException;
+import com.example.demo.common.exception.SysException;
 import com.example.demo.common.response.Response;
 
 /**
- * ResponseHandler
- *
- * @author Frank Zhang
- * @date 2020-11-10 3:18 PM
+ * ResponseHandler - 统一异常处理
  */
-@SuppressWarnings("rawtypes")
 @Component
 public class DefaultResponseHandler {
     private static final Logger log = LogManager.getLogger(DefaultResponseHandler.class);
     
-    public Object handle(Class returnType, String errCode, String errMsg) {
-        // 如果返回类型已经是Response，直接返回错误响应
-        if (Response.class.isAssignableFrom(returnType)) {
-            return Response.error(errCode, errMsg);
+    public Object handle(Class<?> returnType, Throwable e) {
+        if (e instanceof BizException) {
+            // 业务异常
+            BizException biz = (BizException) e;
+            log.warn("业务异常: {}", biz.getMessage());
+            return Response.error(biz.getErrCode(), biz.getMessage());
+        } 
+        else if (e instanceof DataAccessException) {
+            // 数据库相关异常
+            log.error("数据库访问异常: {}", e.getMessage(), e);
+            return Response.error("DB_ERROR", "数据库访问异常");
         }
-        // 如果是其他类型，包装成Response
-        return Response.error(errCode, errMsg);
+        // 网络相关异常
+        else if (e instanceof ResourceAccessException) {
+            // ResourceAccessException 包装了具体的网络异常
+            Throwable cause = e.getCause();
+            if (cause instanceof SocketTimeoutException) {
+                log.error("网络超时: {}", e.getMessage(), e);
+                return Response.error("NETWORK_TIMEOUT", "网络超时，请稍后重试");
+            } else if (cause instanceof ConnectException) {
+                log.error("网络连接失败: {}", e.getMessage(), e);
+                return Response.error("NETWORK_ERROR", "网络连接失败，请检查网络");
+            }
+            // 其他网络异常
+            log.error("网络访问异常: {}", e.getMessage(), e);
+            return Response.error("NETWORK_ERROR", "网络访问异常，请稍后重试");
+        }
+        else if (e instanceof RestClientException) {
+            // 其他REST调用异常
+            log.error("远程服务调用异常: {}", e.getMessage(), e);
+            return Response.error("RPC_ERROR", "远程服务调用异常");
+        }
+        else if (e instanceof SysException) {
+            // 系统异常
+            SysException sys = (SysException) e;
+            log.error("系统异常: {}", sys.getMessage(), sys);
+            return Response.error(sys.getErrCode(), "系统异常，请联系管理员");
+        }
+        // 其他未知异常
+        else {
+            log.error("未知异常: ", e);
+            return Response.error("SYSTEM_ERROR", "系统异常，请联系管理员");
+        }
     }
 }
