535018c405af2ce2e25298cf5595013fd601b65a
mubinlai <71816039@qq.com>
2024-12-09 14:27:45 +0800

    移除冗余的提示

--- src/main/java/com/example/demo/business/product/domain/service/InventoryService.java
+++ src/main/java/com/example/demo/business/product/domain/service/InventoryService.java
@@ -11,30 +11,7 @@ import com.example.demo.common.catchall.CatchAndLog;
 @CatchAndLog
 public class InventoryService {
 
-    // Injecting the InventoryRepo dependency
-    @Autowired
-    private InventoryRepository repository;
-
-    // Set the sale mode for a bond
-    public void setSaleMode(String bondCode, String orgId) {
-        // Business logic to set the sale mode
-        System.out.println("Setting sale mode for bond: " + bondCode + " at org: " + orgId);
-        // Assume this method interacts with the repository and returns a long value
-    }
-
-    // Transfer inventory from one org to another
-    public void transferInventory(String bondCode, long quantity, String fromOrg, String toOrg) {
-        // Business logic to transfer inventory between organizations
-        System.out.println("Transferring " + quantity + " units of bond: " + bondCode + " from org: " + fromOrg + " to org: " + toOrg);
-        // Perform the transfer using the repository
-    }
-
-    // Query inventory for a given bond and organization
-    public Inventory queryInventory(String bondCode, String orgId) {
-        // Business logic to query inventory
-        System.out.println("Querying inventory for bond: " + bondCode + " at org: " + orgId);
-        // Assume the return type 'type' refers to some object type, adjust accordingly
-        return null;
-    }
+    
+    
 }
 
